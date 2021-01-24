7717eaa52f373b34b72bc1ecf75524bf347c7086
Evelyn Hathaway <git@evelyn.dev>
2021-01-24 14:06:27 -0800

    Fix: Save worker in case the page is hot reloaded

--- src/utils/setup-canvas.ts
+++ src/utils/setup-canvas.ts
@@ -6,18 +6,22 @@ import {sortImage} from "./sort";
 
 export type RenderTrigger = (...args: Parameters<typeof sortImage>) => void;
 
+let worker: Worker;
+
 export const setUpCanvas = function (
 	{current: canvasElement}: React.RefObject<HTMLCanvasElement>,
 ): undefined | RenderTrigger {
 	if (!canvasElement) return;
 
 	const hasOffscreen = "OffscreenCanvas" in window;
-	let worker: Worker, offscreenCanvas: OffscreenCanvas, canvasContext: CanvasRenderingContext2D | null;
+	let offscreenCanvas: OffscreenCanvas, canvasContext: CanvasRenderingContext2D | null;
 	if (hasOffscreen) {
 		// Transfer to a worker if there is support to handle off the main thread
-		worker = new Worker();
-		offscreenCanvas = canvasElement.transferControlToOffscreen();
-		worker.postMessage({offscreenCanvas}, [offscreenCanvas]);
+		if (!worker) {
+			worker = new Worker();
+			offscreenCanvas = canvasElement.transferControlToOffscreen();
+			worker.postMessage({offscreenCanvas}, [offscreenCanvas]);
+		}
 	} else {
 		// New canvas context, allow alpha for initial styles
 		canvasContext = canvasElement.getContext("2d");
